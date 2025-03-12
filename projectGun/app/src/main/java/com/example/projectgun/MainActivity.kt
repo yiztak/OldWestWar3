3ebb4463b152fb0221449b157ce11db7f695e927
yiztak <yonosoyyiztak@hotmail.com>
2025-03-12 14:32:02 -0600

    Programa ejemplo base

--- projectGun/app/src/main/java/com/example/projectgun/MainActivity.kt
+++ projectGun/app/src/main/java/com/example/projectgun/MainActivity.kt
@@ -1,74 +1,68 @@
 package com.example.projectgun
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.projectgun.ui.theme.ProjectGunTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ProjectGunTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            UIPrincipal()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UIPrincipal() {
+    val contexto = LocalContext.current
+    var nombre by rememberSaveable { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Nombre:")
+
+            OutlinedTextField(
+                value = nombre,
+                onValueChange = { nombre = it },
+                label = { Text("Introduce tu nombre") },
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth(0.8f)
+            )
+
+            Button(
+                onClick = { Toast.makeText(contexto, "Hola $nombre!!", Toast.LENGTH_LONG).show() },
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text("Saludar!")
+            }
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ProjectGunTheme {
-        Greeting("Android")
-    }
+fun Previsualizacion() {
+    UIPrincipal()
 }
